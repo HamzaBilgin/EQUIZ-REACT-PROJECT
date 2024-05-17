2490c5fb8f1046812002e2865fcdb660f6608fc7
Hamza Bilgin <112509615+HamzaBilgin@users.noreply.github.com>
2024-05-17 22:47:09 +0300

    updated

--- src/components/instructor/QuizConfig.jsx
+++ src/components/instructor/QuizConfig.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-import { auth } from "../../firebaseConfig";
-import { getQuizInfo, updateQuizInfo } from "../../actions/quizActions";
 import { useSelector } from "react-redux";
+import { message, Switch, Tooltip } from "antd";
 import ErrorPage from "../../pages/ErrorPage";
 import ShowAllQuestions from "./ShowAllQuestions";
 import QuizConfigItem from "./QuizConfigItem";
 import QuizConfigDetail from "./QuizConfigDetail";
-import { Switch, Tooltip, message } from "antd";
-let initalQuestion = {
+import { updateQuizInfo } from "../../actions/quizActions";
+
+let initialQuestion = {
   question: "",
   options: [
     { id: "A", value: "" },
@@ -18,24 +18,46 @@ let initalQuestion = {
   ],
   correctOption: "",
 };
+
 const QuizConfig = () => {
   const quiz = useLoaderData();
   const params = useParams();
   const userInfo = useSelector((state) => state.authReducer.user);
-  // console.log(product);
   const [quizInfo, setQuizInfo] = useState(quiz);
   const [arrayIndex, setArrayIndex] = useState(-1);
   const [err, setErr] = useState();
   const [showAllQuestion, setShowAllQuestion] = useState(true);
   const [questionInfo, setQuestionInfo] = useState({});
   const [messageApi, contextHolder] = message.useMessage();
-  const [disabled, setDisabled] = useState(false);
-  const [checked, setChecked] = useState(true);
+  const [statuInfo, setStatuInfo] = useState({});
+
   const infoMessageConfig = (type, content) => {
     messageApi.open({
       type: type,
       content: content,
     });
+  };
+
+  const checkQuestionArray = (data) => {
+    return data.options.some((option) => option.value === "");
+  };
+
+  const changeStatuInfo = async (statutype) => {
+    console.log(statutype);
+    switch (statutype) {
+      case "Incomplete":
+        setStatuInfo({ disabled: true, checked: false, type: "Incomplete" });
+        break;
+      case "Pasif":
+        setStatuInfo({ disabled: false, checked: false, type: "Pasif" });
+        break;
+      case "Active":
+        setStatuInfo({ disabled: false, checked: true, type: "Active" });
+        break;
+      default:
+        setStatuInfo({ disabled: false, checked: false, type: "Pasif" });
+        break;
+    }
   };
 
   useEffect(() => {
@@ -49,38 +71,23 @@ const QuizConfig = () => {
     if (quiz.instructorId.id !== userInfo) {
       const error = new Error("Bu quiz'e erişim izniniz yok");
       error.code = 1;
-
       setErr(error);
     }
 
-    const isEmpty = quiz.questions.some((question) =>
-      question.options.some((option) => option.value === "")
-    );
-    console.log(isEmpty);
-    switch (quiz.statu) {
-      case "Incomplete":
-        setDisabled(true);
-        setChecked(false);
-        break;
-      case "Pasif":
-        setDisabled(false);
-        setChecked(false);
-        break;
-      case "Active":
-        setDisabled(false);
-        setChecked(true);
-        break;
-      default:
-        setDisabled(true);
-        break;
-    }
-  }, [userInfo]);
+    const initialStatus = quizInfo.questions.some(checkQuestionArray)
+      ? "Incomplete"
+      : "Pasif";
+    setQuizInfo({ ...quizInfo, statu: initialStatus });
+    changeStatuInfo(initialStatus);
+  }, [userInfo, quizInfo.questions]);
+
   useEffect(() => {
-    const fetchData = async () => {
+    const updateStatu = async () => {
       await updateQuizInfo(params.quizId, quizInfo);
     };
-    fetchData();
+    updateStatu();
   }, [quizInfo]);
+
   useEffect(() => {
     setQuestionInfo(quizInfo.questions[arrayIndex]);
   }, [arrayIndex]);
@@ -91,104 +98,121 @@ const QuizConfig = () => {
         Error: <ErrorPage />
       </div>
     );
-  const handleLiCLick = (index) => {
+
+  const handleLiClick = (index) => {
     setQuestionInfo(quizInfo.questions[index]);
     setShowAllQuestion(false);
     setArrayIndex(index);
   };
+
   const addEmptyQuestion = () => {
-    setQuizInfo({
+    const newQuizInfo = {
       ...quizInfo,
-      questions: [...quizInfo.questions, initalQuestion],
-    });
+      questions: [...quizInfo.questions, initialQuestion],
+      statu: "Inactive",
+    };
+    setQuizInfo(newQuizInfo);
+    changeStatuInfo("Inactive");
   };
+
   const seeAllQuestionsFunk = () => {
     setShowAllQuestion(true);
     setArrayIndex(-1);
   };
+
   const updateQuestionArray = (data) => {
     const isEmpty = checkQuestionArray(data);
-    console.log(isEmpty);
     if (isEmpty) {
       infoMessageConfig("error", "Nooo");
     } else {
-      setQuizInfo({
+      const updatedQuizInfo = {
         ...quizInfo,
         questions: quizInfo.questions.map((question, i) =>
           i === arrayIndex ? data : question
         ),
-      });
+        statu: "Pasif",
+      };
+      setQuizInfo(updatedQuizInfo);
+      changeStatuInfo("Pasif");
       infoMessageConfig("success", "Question updated");
     }
   };
-  const checkQuestionArray = (data) => {
-    const isEmpty = data.options.find((item) => {
-      return item.value === "";
-    });
-    return isEmpty;
-    // infoMessageConfig("success", "Question updated");
-  };
+
   const deleteQuestion = (deleteIndex) => {
     const updatedQuestions = quizInfo.questions.filter(
       (_, index) => index !== deleteIndex
     );
-    setQuizInfo({
+    const updatedQuizInfo = {
       ...quizInfo,
       questions: updatedQuestions,
-    });
+    };
+    setQuizInfo(updatedQuizInfo);
     setArrayIndex(deleteIndex - 1);
     infoMessageConfig("success", "Question deleted");
   };
+
   const onChange = () => {
-    setChecked(!checked);
+    const newStatus = quizInfo.statu === "Active" ? "Pasif" : "Active";
+    setStatuInfo({ ...statuInfo, checked: newStatus === "Active" });
+    setQuizInfo({
+      ...quizInfo,
+      statu: newStatus,
+    });
   };
+
   return (
     <div className="mt-[70px] max-w-screen-xl w-full m-auto">
       <div className="bg-slate-200 flex justify-between ">
-        <div className="h-16 w-1/3  flex items-center">
+        <div className="h-16 w-1/3 flex items-center">
           <Tooltip
             className="ml-6 flex"
-            title="Bu bir örnek tooltip içeriğidir"
+            title={`${
+              quizInfo.statu === "Incomplete"
+                ? `Some questions are not completed`
+                : ""
+            }`}
           >
-            <Switch disabled={disabled} checked={checked} onChange={onChange} />
+            <Switch
+              disabled={statuInfo.disabled}
+              checked={statuInfo.checked}
+              onChange={onChange}
+            />
             <div className="text-center ml-2">
               <span>Statu : {quizInfo.statu}</span>
             </div>
           </Tooltip>
         </div>
-        <div className="h-16 w-1/3  flex justify-center items-center">
+        <div className="h-16 w-1/3 flex justify-center items-center">
           {quiz.quizName}
         </div>
-
         <div className="h-16 w-1/3 text-end">{`Live Quiz Id : ${quiz.liveQuizId}`}</div>
       </div>
       <div className="mt-5 flex ">
-        <div className=" w-1/6 flex flex-col items-center">
+        <div className="w-1/6 flex flex-col items-center">
           <div
-            className={`text-center p-2 cursor-pointer  w-4/5 mb-2 rounded-full border-2 hover:bg-lime-500 ${
+            className={`text-center p-2 cursor-pointer w-4/5 mb-2 rounded-full border-2 hover:bg-lime-500 ${
               showAllQuestion && "bg-yellow-500"
             }`}
             onClick={seeAllQuestionsFunk}
           >
             SEE ALL QUESTION
           </div>
-          <ul className="text-center flex flex-col justify-center items-center w-1/2 ">
+          <ul className="text-center flex flex-col justify-center items-center w-1/2">
             {quizInfo.questions.map((item, index) => (
               <li
                 key={index}
                 className={`cursor-pointer mb-2 w-full rounded-full hover:bg-lime-500 ${
                   checkQuestionArray(item) ? "text-red-600" : "text-black"
                 } ${arrayIndex === index && "bg-lime-200"}`}
-                onClick={() => handleLiCLick(index)}
+                onClick={() => handleLiClick(index)}
               >
                 {index + 1}.soru
               </li>
             ))}
           </ul>
-
           <button
-            className=" py-1 px-6 mt-2 w-4/5 rounded-full border-2  hover:bg-lime-500"
-            onClick={() => addEmptyQuestion()}
+            className="py-1 px-6 mt-2 w-4/5 rounded-full border-2 hover:bg-lime-500"
+            onClick={addEmptyQuestion}
           >
             Add Question
           </button>
